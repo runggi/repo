2cd77944918e2608de886460b4c79c1832bc6423
owo10 <owo10@DESKTOP-Q64OBT7>
2022-01-24 21:25:27 +0900

    Commit message

---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,36 @@
+package HelloWorld;
+
+import java.util.*;
+import java.io.*;
+
+public class Hello {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int[] arr = new int[9];
+		int i = 0;
+		int num=9;
+		while(true) {
+			st = new StringTokenizer(br.readLine(), "\n");
+			arr[i]=Integer.parseInt(st.nextToken());
+			num--;
+			
+			if(num==0) break;
+			else i++;
+			
+		}
+		int max=0;
+		
+		int a=0;
+		int cnt=0;
+		for(int val: arr) {
+			cnt++;
+			if(val>max) {
+				max=val;
+				a=cnt;
+			}
+		}
+		System.out.println(max);
+		System.out.println(a);
+	}
+}
